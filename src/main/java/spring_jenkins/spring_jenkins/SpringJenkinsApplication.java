91d5bf0206a82bedf936e1d27c2ad3282b524ef3
NAVEEN RAJ <naveenthammisetty979@gmail.com>
2026-07-20 18:13:57 +0530

    First Commit

---
+++ src/main/java/spring_jenkins/spring_jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,24 @@
+package spring_jenkins.spring_jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+   public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Spring Boot Application Started...");
+    }
+
+    public static void main(String[] args) {
+    	logger.info("Spring Boot Application Executed...");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+    }
+}
